298c29a4ec9a8ca56624f6d695774c6cdfe49e77
WhyGuy44 <138849902+WhyGuy44@users.noreply.github.com>
2024-03-11 11:40:07 +1100

    blank site

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import profile from "./assets/images/profile.jpg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-teal-50 h-screen p-10">
+      <div className="w-full">
+        <div className="mx-auto flex w-fit">
+          <img
+            src={profile}
+            className="h-40 shadow-xl rounded-full overflow-hidden "
+            alt="Profile Picture"
+          />
+          <div className="flex flex-col py-10 pl-5 gap-2">
+            <div className="font-redhat font-light text-6xl">Samuel Tint</div>
+            <hr className="border-black"></hr>
+            <div className="pl-1 font-redhat text-xl">Sydney based Software Engineer</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
